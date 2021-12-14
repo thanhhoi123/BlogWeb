e9526b78aa494821ec402441977c5cce360277fe
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2021-12-14 21:04:04 +0700

    .

--- Fakebook/Scripts/activate.bat
+++ Fakebook/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\2021-hk2\LTPython\code\BTNhom\Python-BackEnd\Fakebook
+set VIRTUAL_ENV=D:\Python\BlogWeb\Fakebook
 
 if not defined PROMPT set PROMPT=$P$G
 
