bbfcc4a4e116bb15f57f8359072ebcfaf50f4080
minhquanhbt <minhquanhbt@gmail.com>
2021-12-14 13:07:18 +0700

    Update
    add views route and register route, add file to deploy on heruoku

--- initial.py
+++ initial.py
@@ -15,9 +15,11 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    
+
+    from views import views
     from auth import auth
 
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from models import User, Post, Comment, Follow
@@ -37,4 +39,4 @@ def create_app():
 def create_database(app):
     if not path.exists('PYTHON_FLASK_SOCIAL_NETWORK/' + DB_NAME):
         db.create_all(app=app)
-        print('Created Database!')
+        print('Database Created!')
---
+++ wsgi.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from initial import create_app
+
+app = create_app()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404/index.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500/index.html'), 500
+
+if __name__ == '__main__':
+   app.run(debug = True, port = 80)
